73d27fc84dd22127184f63371349b1d6bd8b5e15
LEETAEHO <tae1560@gmail.com>
2013-04-23 04:08:46 +0900

    Added pagenation on towns

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.9'
 gem 'mysql2'
 gem 'therubyracer', '~> 0.10.0'
 gem 'nokogiri'
+gem 'will_paginate', '~> 3.0'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,9 +1,11 @@
 class TownsController < ApplicationController
   def index
     @towns = Town.all
+    #@towns = paginate(:page => params[:page])
   end
 
   def show
     @town = Town.find(params[:id])
+    @houses = @town.houses.order("confirm_date DESC").paginate(:page => params[:page])
   end
 end
